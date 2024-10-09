756ad7767b3a72c62fc52674e3e77edb3b7dd3c3
Hanna Kuzmynska <kuzminskaiaann@gmail.com>
2024-10-10 00:39:34 +0300

    after render

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import dotenv from 'dotenv';
-dotenv.config(); 
+dotenv.config();
 
 import { initDBConnection } from './db/initMongoConnection.js';
+import { setupServer } from './server.js';  
 
 async function start() {
   try {
     await initDBConnection();
+    setupServer();
   } catch (error) {
     console.error("Error starting the server:", error);
   }
 }
 
 start();
-
