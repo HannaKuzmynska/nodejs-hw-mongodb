a674afbabc6db21938eeb4f66e92f06154ff73b4
Hanna Kuzmynska <kuzminskaiaann@gmail.com>
2024-10-13 11:22:34 +0300

    fix after second check

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { initDBConnection } from './db/initMongoConnection.js';
-import { setupServer } from './server.js';  
+import { setupServer } from './server.js';
+import { loadInitialContacts } from './utils/loadInitialContacts.js';
 
 async function start() {
   try {
     await initDBConnection();
+    await loadInitialContacts(); 
     setupServer();
   } catch (error) {
     console.error("Error starting the server:", error);
