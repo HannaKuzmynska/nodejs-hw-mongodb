fed92be1e0f637f491d085bb9b71ddd90ace9f05
Hanna Kuzmynska <kuzminskaiaann@gmail.com>
2024-10-19 20:30:52 +0300

    fix after check

--- src/models/contact.js
+++ src/models/contact.js
@@ -3,17 +3,28 @@ import mongoose from 'mongoose';
 const contactSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Name is required'],
+  },
+  phoneNumber: {
+    type: String,
+    required: [true, 'Phone number is required'],
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    match: [/.+@.+\..+/, 'Please fill a valid email address'],
   },
-  phone: {
+  isFavourite: {
+    type: Boolean,
+    default: false,
+  },
+  contactType: {
     type: String,
+    enum: ['work', 'home', 'personal'],
     required: true,
-  },
-});
+    default: 'personal',
+  }
+}, { timestamps: true, versionKey: false });
+
 const Contact = mongoose.model('Contact', contactSchema);
+
 export default Contact;
