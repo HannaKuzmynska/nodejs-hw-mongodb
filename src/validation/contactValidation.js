d88b078f405dcb257448e6a83bf43009a1d3f6d5
Hanna Kuzmynska <kuzminskaiaann@gmail.com>
2024-11-17 20:58:15 +0200

    update  contactValidation

--- src/validation/contactValidation.js
+++ src/validation/contactValidation.js
@@ -1,8 +1,10 @@
 import Joi from 'joi';
+
 export const contactJoiSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  contactType: Joi.string().valid('work', 'home', 'personal', 'other').required(),
+  photo: Joi.string().optional() 
 });
