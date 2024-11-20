515f512b4db86ed13998f1862a446ed8726d8dfe
Hanna Kuzmynska <kuzminskaiaann@gmail.com>
2024-11-20 20:51:00 +0200

    change server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,35 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { UPLOAD_DIR } from './constants/index.js';
 import dotenv from 'dotenv';
 import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
-import errorHandler from './middlewares/errorHandler.js';
-import notFoundHandler from './middlewares/notFoundHandler.js';
-import { UPLOAD_DIR } from './constants/index.js';
-import { swaggerDocs } from './middlewares/swaggerDocs.js'
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
 
+dotenv.config(); // Завантажує .env файл
 
-dotenv.config();
+const PORT = Number(env('PORT', '3000'));
 
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/uploads', express.static(UPLOAD_DIR));
-app.use('/api-docs', swaggerDocs());
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  })
+);
+
+app.use('/uploads', express.static(UPLOAD_DIR)); // Статичні файли для завантажених зображень
+app.use('/api-docs', swaggerDocs()); // Swagger документація
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Contacts API');
@@ -25,13 +38,15 @@ app.get('/', (req, res) => {
 app.use('/auth', authRouter);
 app.use('/contacts', contactsRouter);
 
-app.use(notFoundHandler);
+app.use('*', notFoundHandler);
 app.use(errorHandler);
 
-export const setupServer = () => {
-  const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
+export const setupServer = () => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
   });
 };
