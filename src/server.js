389ffe32482eb84ada804423103530b76b4585d2
Hanna Kuzmynska <kuzminskaiaann@gmail.com>
2024-11-20 21:09:07 +0200

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,49 +1,48 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 import { env } from './utils/env.js';
 import { UPLOAD_DIR } from './constants/index.js';
-import dotenv from 'dotenv';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import authRouter from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
-import { swaggerDocs } from './middlewares/swaggerDocs.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import cookieParser from 'cookie-parser';
 
-dotenv.config(); // Завантажує .env файл
-
+dotenv.config();
 const PORT = Number(env('PORT', '3000'));
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
+// Swagger API Documentation
+app.use('/api-docs', swaggerDocs());
+
+// Static file serving
+app.use('/uploads', express.static(UPLOAD_DIR));
+
+// Routes
+app.use('/auth', authRouter);
+app.use('/contacts', contactsRouter);
+
+// Error handling
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+// Logging middleware (pino)
 app.use(
   pino({
     transport: {
       target: 'pino-pretty',
     },
-  })
+  }),
 );
-
-app.use('/uploads', express.static(UPLOAD_DIR)); // Статичні файли для завантажених зображень
-app.use('/api-docs', swaggerDocs()); // Swagger документація
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the Contacts API');
-});
-
-app.use('/auth', authRouter);
-app.use('/contacts', contactsRouter);
-
-app.use('*', notFoundHandler);
-app.use(errorHandler);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
 
 export const setupServer = () => {
   app.listen(PORT, () => {
